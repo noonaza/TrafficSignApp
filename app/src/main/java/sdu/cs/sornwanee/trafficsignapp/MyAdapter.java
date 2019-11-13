8822df0964e5e77a3e61939e8be003e76cd9d33c
sornwanee <sornwanee25402540@gmail.com>
2019-11-13 15:26:39 +0700

    MyAdapter Complete

---
+++ app/src/main/java/sdu/cs/sornwanee/trafficsignapp/MyAdapter.java
@@ -0,0 +1,52 @@
+package sdu.cs.sornwanee.trafficsignapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyAdapter extends BaseAdapter {
+    Context context;
+    int[] ints;
+    String[] titleStrings,detailStrings;
+
+    public MyAdapter(Context context, int[] ints, String[] titleStrings, String[] detailStrings) {
+        this.context = context;
+        this.ints = ints;
+        this.titleStrings = titleStrings;
+        this.detailStrings = detailStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return ints.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_listview,viewGroup,false);
+
+        ImageView iconImageView = view1.findViewById(R.id.imv_icon);
+        TextView titleTextView = view1.findViewById(R.id.tv_title);
+        TextView detailTextView = view1.findViewById(R.id.tv_detail);
+
+        iconImageView.setImageResource(ints[i]);
+        titleTextView.setText(titleStrings[i]);
+        detailTextView.setText(detailStrings[i]);
+        return view1;
+    }
+} //end class
